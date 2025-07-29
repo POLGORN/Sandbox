6e134ec478fd126c7f459ce7b8a4a25e033fe900
POLGORN <polgorn9844@gmail.com>
2025-07-29 09:54:34 +0300

    new file:   mPython/Recursive_search/input.xml 	new file:   mPython/Recursive_search/main.py 	new file:   mPython/Recursive_search/output.txt

---
+++ mPython/Recursive_search/main.py
@@ -0,0 +1,32 @@
+import xml.etree.ElementTree as ET
+import re
+
+def extract_data_recursive(elem, extracted_data):
+    if elem.text:
+        matches = re.findall(r'!(.*?)~', elem.text)
+        extracted_data.extend(matches)
+
+    for attr in elem.attrib.values():
+        matches = re.findall(r'!(.*?)~', attr)
+        extracted_data.extend(matches)
+
+    for child in elem:
+        extract_data_recursive(child, extracted_data)
+
+def main(xml_file, output_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    extracted_data = []
+
+    extract_data_recursive(root, extracted_data)
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(', '.join(extracted_data))
+
+    print(f"Данные записаны в {output_file}")
+
+xml_file = 'input.xml'
+output_file = 'output.txt'
+
+main(xml_file, output_file)
